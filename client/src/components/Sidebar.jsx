49f9b54251aa2af17fbcc2aafbf3e6a13160a817
andrianggoro21 <andri.anggoro21@gmail.com>
2024-06-24 01:26:49 +0700

    fix (client): update sidebar and chatpage

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,63 +1,25 @@
-// import { Box, VStack, useColorMode } from "@chakra-ui/react";
-// import PropTypes from "prop-types";
-// import UserItem from "./UserItem";
-
-// const Sidebar = ({ participants, onUserClick }) => {
-//   const { colorMode } = useColorMode();
-
-//   return (
-//     <Box
-//       w="300px"
-//       p={2}
-//       borderRight="1px solid"
-//       borderColor={colorMode === "light" ? "gray.300" : "gray.700"}
-//       bg={colorMode === "light" ? "gray.50" : "gray.900"}
-//       overflowY="auto"
-//     >
-//       <VStack spacing={2}>
-//         {participants.map((participant) => (
-//           <UserItem key={participant.id} participant={participant} onClick={() => onUserClick(participant)} />
-//         ))}
-//       </VStack>
-//     </Box>
-//   );
-// };
-
-// Sidebar.propTypes = {
-//   participants: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.string.isRequired,
-//       name: PropTypes.string.isRequired,
-//     })
-//   ).isRequired,
-//   onUserClick: PropTypes.func.isRequired,
-// };
-
-// export default Sidebar;
-
-import { Box, VStack, useColorMode } from "@chakra-ui/react";
+import { Box, VStack } from "@chakra-ui/react";
 import PropTypes from "prop-types";
 import UserItem from "./UserItem";
 import GroupItem from "./GroupItem"; // Import GroupItem
 
 const Sidebar = ({ participants, onUserClick, onGroupClick, groups }) => {
-  const { colorMode } = useColorMode();
-
   return (
-    <Box
-      w="300px"
-      p={2}
-      borderRight="1px solid"
-      borderColor={colorMode === "light" ? "gray.300" : "gray.700"}
-      bg={colorMode === "light" ? "gray.50" : "gray.900"}
-      overflowY="auto"
-    >
+    <Box>
       <VStack spacing={2}>
         {groups.map((group) => (
-          <GroupItem key={group.id} group={group} onClick={() => onGroupClick(group)} />
+          <GroupItem
+            key={group.id}
+            group={group}
+            onClick={() => onGroupClick(group)}
+          />
         ))}
         {participants.map((participant) => (
-          <UserItem key={participant.id} participant={participant} onClick={() => onUserClick(participant)} />
+          <UserItem
+            key={participant.id}
+            participant={participant}
+            onClick={() => onUserClick(participant)}
+          />
         ))}
       </VStack>
     </Box>
@@ -82,6 +44,3 @@ Sidebar.propTypes = {
 };
 
 export default Sidebar;
-
-
-
--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,6 +1,13 @@
 // import { useEffect, useState } from "react";
 // import axios from "axios";
-// import { Box, Flex, Text, IconButton, useColorMode } from "@chakra-ui/react";
+// import {
+//   Box,
+//   Flex,
+//   Text,
+//   IconButton,
+//   useColorMode,
+//   Image,
+// } from "@chakra-ui/react";
 // import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 // import Message from "../components/Message";
 // import Sidebar from "../components/Sidebar";
@@ -12,21 +19,32 @@
 //   const [messages, setMessages] = useState([]);
 //   const [participants, setParticipants] = useState([]);
 //   const [currentUser, setCurrentUser] = useState(null);
+//   const [currentGroup, setCurrentGroup] = useState(null);
+//   const [groups, setGroups] = useState([]);
+//   const [currentGroupImageUrl, setCurrentGroupImageUrl] = useState("");
 //   const { colorMode, toggleColorMode } = useColorMode();
 
 //   const fetchMessages = async () => {
 //     const response = await axios.get(`${apiUrl}/messages`);
-//     console.log(response);
 //     const data = response.data.results;
-//     const messages = data.comments.map(comment => ({
+//     const messages = data.comments.map((comment) => ({
 //       id: comment.id,
-//       sender: data.room.participant.find(participant => participant.id === comment.sender).name,
+//       sender: data.room.participant.find(
+//         (participant) => participant.id === comment.sender
+//       ).name,
 //       content: comment.message,
 //       type: comment.type,
 //       timestamp: comment.timestamp || new Date().toISOString(),
 //     }));
 //     setMessages(messages);
 //     setParticipants(data.room.participant);
+//     setGroups([
+//       {
+//         id: data.room.id,
+//         name: data.room.name,
+//         image_url: data.room.image_url,
+//       },
+//     ]);
 //   };
 
 //   useEffect(() => {
@@ -35,6 +53,13 @@
 
 //   const handleUserClick = (user) => {
 //     setCurrentUser(user);
+//     setCurrentGroup(null); // Reset current group when user is clicked
+//   };
+
+//   const handleGroupClick = (group) => {
+//     setCurrentGroup(group);
+//     setCurrentGroupImageUrl(group.image_url); // Set current group image url
+//     setCurrentUser(null); // Reset current user when group is clicked
 //   };
 
 //   const filteredMessages = currentUser
@@ -42,11 +67,18 @@
 //         (message) =>
 //           message.sender === currentUser.name || message.sender === "You"
 //       )
+//     : currentGroup
+//     ? messages // Display messages for the current group
 //     : [];
 
 //   return (
 //     <Flex height="100vh" overflow="hidden">
-//       <Sidebar participants={participants} onUserClick={handleUserClick} />
+//       <Sidebar
+//         participants={participants}
+//         onUserClick={handleUserClick}
+//         onGroupClick={handleGroupClick}
+//         groups={groups}
+//       />
 //       <Box flex="1" display="flex" flexDirection="column">
 //         <Flex
 //           p={4}
@@ -57,7 +89,11 @@
 //           justifyContent="space-between"
 //         >
 //           <Text fontWeight="bold">
-//             {currentUser ? currentUser.name : "Select a user to start chatting"}
+//             {currentUser
+//               ? currentUser.name
+//               : currentGroup
+//               ? currentGroup.name
+//               : "Select a user or group to start chatting"}
 //           </Text>
 //           <IconButton
 //             icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
@@ -66,18 +102,28 @@
 //             aria-label="Toggle Color Mode"
 //           />
 //         </Flex>
-//         <Box flex="1" p={4} overflowY="auto" bg={colorMode === "light" ? "white" : "gray.800"}>
-//           {currentUser ? (
+//         <Box
+//           flex="1"
+//           p={4}
+//           overflowY="auto"
+//           bg={colorMode === "light" ? "white" : "gray.800"}
+//         >
+//           {currentGroup && (
+//             <Image src={currentGroupImageUrl} alt="Group" p={2} mb={2} />
+//           )}
+//           {currentUser || currentGroup ? (
 //             filteredMessages.map((message) => (
 //               <Message key={message.id} message={message} />
 //             ))
 //           ) : (
 //             <Box textAlign="center" color="gray.500">
-//               Select a user to start chatting
+//               Select a user or group to start chatting
 //             </Box>
 //           )}
 //         </Box>
-//         {currentUser && <ChatInput fetchMessages={fetchMessages} />}
+//         {(currentUser || currentGroup) && (
+//           <ChatInput fetchMessages={fetchMessages} />
+//         )}
 //       </Box>
 //     </Flex>
 //   );
@@ -87,8 +133,22 @@
 
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Box, Flex, Text, IconButton, useColorMode } from "@chakra-ui/react";
-import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  useColorMode,
+  Image,
+  useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
+import { SunIcon, MoonIcon, HamburgerIcon } from "@chakra-ui/icons";
 import Message from "../components/Message";
 import Sidebar from "../components/Sidebar";
 import ChatInput from "../components/ChatInput";
@@ -99,21 +159,33 @@ const ChatPage = () => {
   const [messages, setMessages] = useState([]);
   const [participants, setParticipants] = useState([]);
   const [currentUser, setCurrentUser] = useState(null);
-  const [currentGroup, setCurrentGroup] = useState(null); // Add this line
+  const [currentGroup, setCurrentGroup] = useState(null);
+  const [groups, setGroups] = useState([]);
+  const [currentGroupImageUrl, setCurrentGroupImageUrl] = useState("");
   const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const fetchMessages = async () => {
     const response = await axios.get(`${apiUrl}/messages`);
     const data = response.data.results;
-    const messages = data.comments.map(comment => ({
+    const messages = data.comments.map((comment) => ({
       id: comment.id,
-      sender: data.room.participant.find(participant => participant.id === comment.sender).name,
+      sender: data.room.participant.find(
+        (participant) => participant.id === comment.sender
+      ).name,
       content: comment.message,
       type: comment.type,
       timestamp: comment.timestamp || new Date().toISOString(),
     }));
     setMessages(messages);
     setParticipants(data.room.participant);
+    setGroups([
+      {
+        id: data.room.id,
+        name: data.room.name,
+        image_url: data.room.image_url,
+      },
+    ]);
   };
 
   useEffect(() => {
@@ -122,12 +194,15 @@ const ChatPage = () => {
 
   const handleUserClick = (user) => {
     setCurrentUser(user);
-    setCurrentGroup(null); // Reset current group when user is clicked
+    setCurrentGroup(null);
+    onClose();
   };
 
   const handleGroupClick = (group) => {
     setCurrentGroup(group);
-    setCurrentUser(null); // Reset current user when group is clicked
+    setCurrentGroupImageUrl(group.image_url);
+    setCurrentUser(null);
+    onClose();
   };
 
   const filteredMessages = currentUser
@@ -136,17 +211,27 @@ const ChatPage = () => {
           message.sender === currentUser.name || message.sender === "You"
       )
     : currentGroup
-    ? messages // Display messages for the current group
+    ? messages
     : [];
 
   return (
     <Flex height="100vh" overflow="hidden">
-      <Sidebar 
-        participants={participants} 
-        onUserClick={handleUserClick} 
-        onGroupClick={handleGroupClick} // Add this line
-        groups={[{ id: 1, name: "Product A" }]} // Example groups
-      />
+      <Box
+        display={{ base: "none", md: "block" }}
+        w="300px"
+        p={2}
+        borderRight="1px solid"
+        borderColor={colorMode === "light" ? "gray.300" : "gray.700"}
+        bg={colorMode === "light" ? "gray.50" : "gray.900"}
+        overflowY="auto"
+      >
+        <Sidebar
+          participants={participants}
+          onUserClick={handleUserClick}
+          onGroupClick={handleGroupClick}
+          groups={groups}
+        />
+      </Box>
       <Box flex="1" display="flex" flexDirection="column">
         <Flex
           p={4}
@@ -156,8 +241,19 @@ const ChatPage = () => {
           alignItems="center"
           justifyContent="space-between"
         >
+          <IconButton
+            display={{ base: "block", md: "none" }}
+            icon={<HamburgerIcon />}
+            onClick={onOpen}
+            variant="ghost"
+            aria-label="Open Menu"
+          />
           <Text fontWeight="bold">
-            {currentUser ? currentUser.name : currentGroup ? currentGroup.name : "Select a user or group to start chatting"}
+            {currentUser
+              ? currentUser.name
+              : currentGroup
+              ? currentGroup.name
+              : "Select a user or group to start chatting"}
           </Text>
           <IconButton
             icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
@@ -166,7 +262,15 @@ const ChatPage = () => {
             aria-label="Toggle Color Mode"
           />
         </Flex>
-        <Box flex="1" p={4} overflowY="auto" bg={colorMode === "light" ? "white" : "gray.800"}>
+        <Box
+          flex="1"
+          p={4}
+          overflowY="auto"
+          bg={colorMode === "light" ? "white" : "gray.800"}
+        >
+          {currentGroup && (
+            <Image src={currentGroupImageUrl} alt="Group" p={2} mb={2} />
+          )}
           {currentUser || currentGroup ? (
             filteredMessages.map((message) => (
               <Message key={message.id} message={message} />
@@ -177,12 +281,31 @@ const ChatPage = () => {
             </Box>
           )}
         </Box>
-        {(currentUser || currentGroup) && <ChatInput fetchMessages={fetchMessages} />}
+        {(currentUser || currentGroup) && (
+          <ChatInput fetchMessages={fetchMessages} />
+        )}
       </Box>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader
+              bg={colorMode === "light" ? "gray.100" : "gray.900"}
+              p={8}
+            ></DrawerHeader>
+            <DrawerBody bg={colorMode === "light" ? "gray.100" : "gray.900"}>
+              <Sidebar
+                participants={participants}
+                onUserClick={handleUserClick}
+                onGroupClick={handleGroupClick}
+                groups={groups}
+              />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
     </Flex>
   );
 };
 
 export default ChatPage;
-
-
