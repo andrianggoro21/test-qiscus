8be5bc1182cbadb952228f8ccc0f951328857bcd
andrianggoro21 <andri.anggoro21@gmail.com>
2024-06-24 11:34:44 +0700

    fix: update readme.md

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ require("dotenv").config({
   path: path.resolve(__dirname, "../.env"),
 });
 const app = new express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
